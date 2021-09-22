3cf317a96e1b4544a4ad61d74798a7d735503f29
Edouard Misset <edouardmisset@gmail.com>
2021-09-22 12:41:47 +0200

    Fetched data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      text: 'React',
+      monsters: [],
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello {this.state.text}</p>
-          <button onClick={() => this.setState({ text: 'Me' })}>
-            Change text
-          </button>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     )
   }
